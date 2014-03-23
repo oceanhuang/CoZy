653ef8d290ba51fdce5ebea749b2428547851b97
Giovanni Ortuno <giovanni.ortuno@outlook.com>
2014-03-23 01:42:43 -0400

    Basic Code Generator

--- codeGenerator.py
+++ codeGenerator.py
@@ -1,3 +1,19 @@
 class codeGenerator(object):
     
-
+    def __init__(self, tree):
+        # Keep track of scopes
+        self.varScopes = [[]]
+        self.scopeDepth = 0
+        # Symbols table
+        self.symbolsTable = {}
+        # Keep track of indentation
+        self._indent = 0
+        self.ret = self.dispatch(tree)
+    
+    def dispatch(self, tree, flag=None):
+        '''Dispatches based on type of node'''
+        method = getattr(self, "_"+tree.type)
+        code = method(tree, flag)
+        return code
+    
+    
