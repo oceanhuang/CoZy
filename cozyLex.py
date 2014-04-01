54c453177aa3bb3ca99a643082c0dc5c73b5bc09
Giovanni Ortuno <giovanni.ortuno@outlook.com>
2014-03-31 21:49:45 -0400

    Cleaned code and set the base lexer and parser to expand

--- cozyLex.py
+++ cozyLex.py
@@ -3,62 +3,46 @@ import ply.lex as lex
 # List of token names.   This is always required
 reserved = {
     'every' : 'EVERY',
-    'Monday' : 'MONDAY',
-    'Tuesday': 'TUESDAY',
-    'Wednesday' : 'WEDNESDAY',
-    'Thursday' : 'THURSDAY',
-    'Friday' : 'FRIDAY',
-    'Saturday' : 'SATURDAY',
-    'Sunday' : 'SUNDAY',
+    # 'Monday' : 'MONDAY',
+    # 'Tuesday': 'TUESDAY',
+    # 'Wednesday' : 'WEDNESDAY',
+    # 'Thursday' : 'THURSDAY',
+    # 'Friday' : 'FRIDAY',
+    # 'Saturday' : 'SATURDAY',
+    # 'Sunday' : 'SUNDAY',
+    'def' : 'DEF',
 }
 
 tokens = [
-   'LPAREN',
-   'RPAREN',
-   'FALSE',
-   'TRUE',
-   'RBRACK',
-   'LBRACK',
-   'NAND',
-
-   'PLUS',
-   'ID',
-   'INT',
-   'SEMICOLON',
-   'EQUALS',
+    'LPAREN',
+    'RPAREN',
+    'RBRACK',
+    'LBRACK',
+    'EQUALS',
+    'PLUS',
+    'MINUS',
+    'ID',
+    'SEMICOLON',
+    'CONSTANT',
 ] + list(reserved.values())
 
 # Regular expression rules for simple tokens
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
-t_LBRACK  = r'\{'
-t_RBRACK  = r'\}'
-t_PLUS    = r'\+'
-t_NAND    = r'nand'
-t_EQUALS  = r'='
+t_LPAREN    = r'\('
+t_RPAREN    = r'\)'
+t_LBRACK    = r'\{'
+t_RBRACK    = r'\}'
+t_EQUALS    = r'='
+t_PLUS      = r'\+'
+t_MINUS     = r'\-'
 t_SEMICOLON = r';'
-
+t_CONSTANT  = r'[0-9]+|((\'|\").*?(\'|\"))'
 # A regular expression rule with some action code
-def t_INT(t):
-    r'[0-9]+'
-    t.value = int(t.value)
-    return t
 
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
     t.type = reserved.get(t.value, 'ID') #looks in reserved list, if can't find, assigns it to type ID 
     return t
-
-def t_TRUE(t):
-    r'true'
-    t.value = True
-    return t
-
-def t_FALSE(t):
-    r'false'
-    t.value = False
-    return t
-
+    
 # Define a rule so we can track line numbers
 def t_newline(t):
     r'\n+'
@@ -76,7 +60,11 @@ def t_error(t):
 lexer = lex.lex()
 
 # Test it out
-data = 'x=3+3'
+data = """
+x=3+3;
+y = 2 + "every";
+z = Tuesday;
+"""
 
 # Give the lexer some input
 lexer.input(data)
--- cozyYacc.py
+++ cozyYacc.py
@@ -1,11 +1,15 @@
 import ply.yacc as yacc
 
 # Get the token map from the lexer.  This is required.
-from cozyAddLex import tokens
+from cozyLex import tokens
 from codeGenerator import *
 
-indent_level = 0
 class Node(object):
+    """ Node class. Used to build the AST. Each node has a type,
+    children and a leaf. If it has a leaf it has *no* children.
+    """
+    # Function to initialize the node, needs type, the rest is
+    # optional.
     def __init__(self, type, children=None, leaf=None):
         self.type = type
         if children:
@@ -14,10 +18,10 @@ class Node(object):
             self.children = [ ]
         self.leaf = leaf
     
-    #when you print a node, print the tree traversal of it
+    # Function to print out the tree.
     def __str__(self):
         return "\n" + self.traverse(1)
-
+    # Function to traverse the tree and print it out.
     def traverse(self, i):
         temp = ""
         if self.leaf:
@@ -28,64 +32,84 @@ class Node(object):
             s += "-"*(i-1) + ">" + children.traverse(i+1)
         return s
 
-def p_prof_stmtseq(p):
-    'prog : stmtseq'
-    p[0] = Node("stmtseq", [p[1]])
+def p_program(p):
+    """ program : external_declaration
+                | program external_declaration
+    """
+    if len(p) == 2:
+        p[0] = Node("program", [p[1]])
+    else:
+        p[0] = Node("program", [p[1], p[2]])
 
-def p_stmtseq_stmt(p):
-    'stmtseq : stmt'
-    p[0] = Node("stmt", [p[1]])
+def p_external_declaration(p):
+    """ external_declaration : function_definition
+                             | statement
+    """
+    p[0] = Node("external_declaration", [p[1]])
 
-def p_stmtseq_stmtseq(p):
-    'stmtseq : stmtseq stmt'
-    p[0] = Node("stmtseq_stmt", [p[1], p[2]])
+# Needs to include parameter_list
+def p_function_definition(p):
+    'function_definition : DEF ID LPAREN RPAREN LBRACK statement_list RBRACK'
 
-def p_stmt_every(p):
-    'stmt : EVERY LPAREN timeexpr RPAREN LBRACK stmtseq RBRACK'
-    p[0] = Node('every', [p[3], p[6]])
+    p[0] = Node("function_definition", [p[2], p[4], p[7]]);
 
-def p_stmt_id(p):
-    'stmt : ID EQUALS expr SEMICOLON'
-    p[0] = Node('stmt_assign', [p[3]], p[1])
+def p_statement_list(p):
+    """ statement_list : statement
+                       | statement_list statement
+    """
+    if len(p) == 2:
+        p[0] = Node("statement_list", [p[1]])
+    else:
+        p[0] = Node("statement_list", [p[1], p[2]])
 
-def p_expr_plus(p):
-    'expr : expr PLUS factor'
-    p[0] = Node('plus', [p[1], p[3]]);
+# Add types of statements here, e.g. selection, iteration, etc!
+def p_statement(p):
+    """ statement : assignment_statement  SEMICOLON
+                  | every_statement
+    """
+    p[0] = Node("statement", [p[1]])
 
-def p_expr_factor(p):
-    'expr : factor'
-    p[0] = Node('expr', [p[1]])
-    #p[0] = p[1]
+# Modify this to go to 'or_expression' once the expression depending
+# on it are finished
+def p_assignment_statement(p):
+    """ assignment_statement : ID EQUALS additive_expression 
+                             | ID EQUALS assignment_statement
+    """
+    
+    if len(p) == 4:
+        p[0] = Node("assignment_statement", [p[1], p[3]])
+    else:
+        p[0] = Node("assignment_statement", [p[1]])
+# Change to continue sequence in grammar i.e. multiplicative_expression, etc
+def p_additive_expresion(p):
+    """ additive_expression : primary_expression
+                             | additive_expression PLUS primary_expression
+                             | additive_expression MINUS primary_expression
+    """
+    if len(p) == 2:
+        p[0] = Node("additive_expression", [p[1]])
+    else:
+        p[0] = Node("additive_expression", [p[1], p[3], p[2]])
 
-def p_factor_parenth(p):
-    'factor : LPAREN expr RPAREN'
-    p[0] = Node('factor', [p[2]]) 
-    #p[0] = '(' + p[2] + ')'
+# Change to include arrays
+def p_primary_expression(p):
+    """ primary_expression : CONSTANT
+                           | ID
+                           | LPAREN additive_expression RPAREN
+    """
+    if not isinstance(p[1], basestring) and not isinstance(p[1], int):
+        p[0] = Node('primary_expression', [p[1]])
+    else:
+        p[0] = Node('primary_expression', [], p[1])
 
-def p_factor_INT(p):
-    'factor : INT'
-    p[0] = Node('factor', [ ], `p[1]`)
-    #p[0] = `p[1]`
-
-def p_factor_id(p):
-    'factor : ID'
-    p[0] = Node('factor', [ ], p[1])
-
-def p_timeexpr_mon(p):
-    '''timeexpr : MONDAY
-                | TUESDAY
-                | WEDNESDAY
-                | THURSDAY
-                | FRIDAY
-                | SATURDAY
-                | SUNDAY
-    '''
-    p[0] = Node('timeexpr', [ ], p[1])
-
+def p_every_statement(p):
+    """ every_statement : EVERY LPAREN additive_expression RPAREN LBRACK statement_list RBRACK """
+    p[0] = Node("every_statement", [p[3], p[6]])
 
 # Error rule for syntax errors
 def p_error(p):
     print "Syntax error in input!"
+    print p
 
 # Build the parser
 parser = yacc.yacc()
@@ -98,5 +122,5 @@ while True:
    if not s: continue
    result = parser.parse(s)
    code = codeGenerator(result)
-##   print result
+   print result
    print code.ret
