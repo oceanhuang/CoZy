68d8ec008b0f1b6d509535ee7c806478bdcf3f56
oceanhuang <ocean.m.huang@gmail.com>
2014-04-20 23:25:03 -0400

    more test cases for date time

--- test/testCoZy.py
+++ test/testCoZy.py
@@ -112,3 +112,19 @@ every (Monday):
     print ('5')
 '''
 myTester.run_code(s, 5)
+
+# test datetime
+s='''
+ret = 21/7/1991 10:00 PM
+ret = 2:00 PM
+ret = 19/12/1991
+'''
+myTester.run_code(s, None)
+
+# test temp
+s='''
+ret = 70 K
+ret = 50 C
+ret = 80 F
+'''
+myTester.run_code(s, None)
