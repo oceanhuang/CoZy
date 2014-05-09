049a54cbfe6631511758b01a17ed972a7972e6bc
oceanhuang <ocean.m.huang@gmail.com>
2014-05-09 02:06:20 -0400

    fixed dealing with float in test

--- test/testCoZy.py
+++ test/testCoZy.py
@@ -25,10 +25,9 @@ class CoZyTester:
         if output != "None":
             result = locals()['ret']
             if type(result) is int: result = str(result)
+            if type(result) is float: result = str(result)
             if type(result) is list: output = eval(output)
             assert result == output
-
-
 
 prog = ''
 arg = ''
