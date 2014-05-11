e94830905b00aa3a07e16bdec2d7e0c626915c7c
oceanhuang <ocean.m.huang@gmail.com>
2014-05-11 11:04:23 -0400

    made ThermoStat return a Temperature object

--- r_pi/Fake_ThermoStat.py
+++ r_pi/Fake_ThermoStat.py
@@ -1,3 +1,31 @@
+class Temperature:
+    def __init__(self, number, tempType):
+        self.startType = tempType
+        if tempType == 'K':
+            self.KTemp = number
+            self.CTemp = number + 273.15
+            self.FTemp = 5.0/9.0*(number - 32.0) + 273.15
+        elif tempType == 'C':
+            self.KTemp = number + 273.15
+            self.CTemp = number
+            self.FTemp = 9.0/5.0*number + 32.0      
+        elif tempType == 'F':
+            self.KTemp = 5.0/9.0*(number - 32.0) + 273.15
+            self.CTemp = 5.0/9.0*(number -32.0)
+            self.FTemp = number
+    def getCelsius(self):
+        return self.CTemp
+    def getFarenheit(self):
+        return self.FTemp
+    def getKelvin(self):
+        return self.KTemp
+    def __str__(self):
+        if self.startType == ' K':
+            return str(self.KTemp) + ' K'
+        elif self.startType == 'C':
+            return str(self.CTemp) + ' C'
+        elif self.startType == 'F':
+            return str(self.FTemp) + ' F'
 
 class Fake_ThermoStat(object):
 
@@ -14,11 +42,19 @@ class Fake_ThermoStat(object):
         print 'Heater off'
         self.on = 0
 
-    def get_temp(self):
+    def get_temp_value(self):
         tfile = open("r_pi/fake_temp") 
         text = tfile.read() 
         tfile.close() 
         temperature = float(text) 
+        return temperature
+
+    def get_temp(self):
+        tfile = open("r_pi/fake_temp") 
+        text = tfile.read() 
+        tfile.close() 
+        temperature = float(text)
+        temperature = Temperature(temperature, 'C')
         return temperature
 
     def set_temp(self, target_temp):
